1c843dfcc1caae47b1f4f10109786c196a4706dd
PRATYUSH MAHAPATRA <18btcse42@suiit.ac.in>
2021-01-13 15:17:35 +0530

    Update App.js

--- emoji/src/App.js
+++ emoji/src/App.js
@@ -9,6 +9,8 @@ var emojiDictionary = {
   "😑": "annoyance"
 };
 
+var emojisWeKnow = Object.keys(emojiDictionary);
+
 export default function App() {
   const [meaning, setMeaning] = useState("");
 
@@ -18,9 +20,14 @@ export default function App() {
     var meaning = emojiDictionary[userInput];
 
     if (meaning === undefined) {
-      meaning = "we dont have any emoji like this in our database";
+      meaning = "not present in our database";
     }
     setMeaning(meaning);
+  }
+  function emojiClickHandler(emoji) {
+    // processing
+    var meaning = emojiDictionary[emoji];
+    setMeaning(meaning); // react call to show output
   }
 
   return (
@@ -30,6 +37,18 @@ export default function App() {
       <input onChange={emojiInputHandler}></input>
 
       <h2>{meaning}</h2>
+      <h3> emojis we know </h3>
+      {emojisWeKnow.map(function (emoji) {
+        return (
+          <span
+            onClick={() => emojiClickHandler(emoji)}
+            style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
+            key={emoji}
+          >
+            {emoji}
+          </span>
+        );
+      })}
     </div>
   );
 }
